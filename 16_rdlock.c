a0db2e639a278ce99a7e810d1902642bfbf15295
Dhruva Sharma <dhruva1098@gmail.com>
2024-08-20 10:54:17 +0530

    15, 16 both write and read locks

---
+++ 16_rdlock.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+int main(){
+	int fd = open("example.txt", O_RDWR);
+	struct flock lock;
+	lock.l_type = F_RDLCK;
+	lock.l_whence = SEEK_SET;
+	lock.l_start = 0;
+	lock.l_len = 0;
+	lock.l_pid = getpid();
+	printf("entering cs \n");
+	fcntl(fd,F_SETLKW, &lock);
+	printf("inside the cs \n Press enter to exit cs \n");
+	getchar();
+	lock.l_type = F_UNLCK;
+	fcntl(fd, F_SETLK, &lock);
+	printf("out of cs");
+}
---
+++ ss16_wrlock.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+
+int main(){
+	int fd = open("example.txt", O_RDWR);
+	struct flock lock;
+	lock.l_type = F_WRLCK;
+	lock.l_whence = SEEK_SET;
+	lock.l_start = 0;
+	lock.l_len = 0;
+	lock.l_pid = getpid();
+	printf("entering cs \n");
+	fcntl(fd,F_SETLKW, &lock);
+	printf("inside the cs \nPress enter to exit cs \n");
+	getchar();
+	lock.l_type = F_UNLCK;
+	fcntl(fd, F_SETLK, &lock);
+	printf("exiting the cs\n");
+	printf("out of cs");
+}
