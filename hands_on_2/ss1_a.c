47199b1bd2cd1588a04a559bf0c7d596926c6fff
Dhruva Sharma <dhruva1098@gmail.com>
2024-09-21 20:42:43 +0530

    ss1 a,b,c

---
+++ hands_on_2/ss1_a.c
@@ -0,0 +1,32 @@
+/*
+===========================================================================================
+Name : ss34.c
+Author : Dhruva Sharma
+Description :  Write a separate program (for each time domain) to set a interval timer in
+10sec and 10micro second
+a. ITIMER_REAR
+b. ITIMER_VIRTUAL
+c. ITIMER_PROF
+
+============================================================================================
+*/
+#include <stdio.h>
+#include <sys/time.h>
+#include <signal.h>
+#include <unistd.h>
+
+void handler(int signal) {
+    printf("ITIMER_REAL triggered\n");
+}
+
+int main() {
+    struct itimerval t;
+    signal(SIGALRM, handler);
+    t.it_value.tv_sec = 10;
+    t.it_value.tv_usec = 10;
+    t.it_interval.tv_sec = 10;
+    t.it_interval.tv_usec = 10;
+    setitimer(ITIMER_REAL, &t, NULL);
+    while (1) {}
+    return 0;
+}
